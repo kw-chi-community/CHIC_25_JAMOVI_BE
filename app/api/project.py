57491bd4095680494cd33c45f6cef414047b0e2d
dasol kim <dasolkim7@naver.com>
2025-01-19 23:25:50 +0900

    project.py import 최종 수정정

--- app/api/project.py
+++ app/api/project.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from models import Project, get_db, ProjectPermission
-from .auth import auth_middleware
+from middleware import auth_middleware
 
 router = APIRouter()
 
