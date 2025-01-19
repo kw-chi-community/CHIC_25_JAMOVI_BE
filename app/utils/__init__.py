d2aa5c504e305287dde1fe5c6050fce0b7cc3d9b
ga111o <ga111o@proton.me>
2025-01-19 16:07:58 +0900

    🔊 add logger

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,1 +1,2 @@
 from .security import verify_password, create_access_token, get_password_hash
+from .logger import logger
---
+++ app/utils/logger.py
@@ -0,0 +1,15 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+formatter = logging.Formatter(
+    '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
