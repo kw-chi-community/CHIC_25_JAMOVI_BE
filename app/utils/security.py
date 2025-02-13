028261a5503cb9a80db9158ee3aa9c32ec7a2b14
ga111o <ga111o@proton.me>
2025-02-13 17:49:07 +0900

    token exp 기한 늘렸어요

--- app/utils/security.py
+++ app/utils/security.py
@@ -14,7 +14,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(days=30)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, os.getenv("JWT_SECRET_KEY"), algorithm=os.getenv("JWT_ALGORITHM"))
     return encoded_jwt 
