56e3b1bdbc5e06b8d8d68fc798ce105201603e1b
ga111o <ga111o@proton.me>
2025-02-15 17:48:34 +0900

    add lite llm model

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,3 +1,3 @@
-from .llm import llm
+from .llm import llm, llm_lite
 from .create_results import llm_results
 from .rscripts import one_sample_t_test, independent_t_test, one_way_anova, paired_t_test
--- app/services/llm.py
+++ app/services/llm.py
@@ -11,6 +11,14 @@ llm = GoogleGenerativeAI(
     model="gemini-2.0-flash",
     google_api_key=api_key,
     temperature=0.0,
-    streaming=True)
+    max_output_tokens=8192,
+)
+
+llm_lite = GoogleGenerativeAI(
+    model="gemini-2.0-flash-lite-preview-02-05",
+    google_api_key=api_key,
+    temperature=0.0,
+    max_output_tokens=8192,
+)
 
 logger.info("llm initialized") 
